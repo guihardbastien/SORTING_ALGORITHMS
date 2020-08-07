c0b26714c8102966329537bf922e1cb815f4edcd
guihardbastien <bastien_guihard@orange.fr>
2020-08-07 16:01:54 +0200

    Feat: bubblesort

--- README.md
+++ README.md
@@ -2,23 +2,52 @@
 ## Description
 Sorting algorithms implementation
 ## Overview
-
+!(sorting_hat)[https://media.giphy.com/media/N3tNKBmQR3K2k/source.gif]
 ### Directory structure
 ```bash
-TBA
+.
+├── errors.ts
+├── index.ts
+├── sorting_algorithms.ts
+└── utils
+    └── index.ts
 ```
 ### How to build the library to be used in production-ready projects?
 ```bash
-TBA
+npm install <lib_name>
 ```
 ### How to use?
 __Example:__
 
 ```typescript
-TBA
+Import * as Algs from 'sorting-algorithms'
+
+const arr: number[] = [7, 5, 2];
+const test = Algs.bubbleSort(arr);
+console.log(test);
+// output: [2, 5, 7]
 ```
 ### Features
 TBA
+
+* Insertion sort:
+    * Worst case: __O(n²)__
+    * Average case: __O(n²)__
+* Selection sort:
+    * Worst case:__O(n²)__
+    * Avergare case:__O(n²)__
+* Heap sort:
+    * Worst case:__O(n log n)__
+    * Avergare case:__O(n log n)__
+* Quick sort:
+    * Worst case:__O(n²)__
+    * Avergare case:__O(n log n)__
+* Bubble sort:
+    * Worst case:__O(n²)__
+    * Avergare case:__O(n²)__
+* Merge sort:
+    * Worst case:__O(n log n)__
+    * Avergare case:__O(n log n)__
 ## NPM custom commands
 
 - `build`: Build the JavaScript files.
---
+++ lib/sorting_algorithms.ts
@@ -0,0 +1,86 @@
+import * as Utils from './utils/index';
+
+/**
+ * Insertion sort
+ */
+export function insertionSort(arr: number[]) {
+    for (let i = 0, len = arr.length; i < len; i += 1) {
+        const key = arr[i];
+        let j = i - 1;
+        while ((j >= 0) && (arr[j] > key)) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+
+/**
+ * Selection sort
+ */
+export function selectionSort(arr: number[]) {
+    for (let i = 0, len = arr.length; i < len; i += 1) {
+        let minimumIndex = i;
+        for (let j = i + 1, len = arr.length; j < len; j += 1) {
+            if (arr[j] < arr[minimumIndex]) {
+                minimumIndex = j;
+            }
+        }
+        Utils.swap(arr, minimumIndex, i);
+    }
+    return arr;
+}
+
+/**
+ * Merge sort
+ */
+export function mergeSort(arr: number[]): any {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    const { left, right } = Utils.divideInTwo(arr);
+    return Utils.merge(mergeSort(left), mergeSort(right));
+}
+
+/**
+ * Heapsort
+ */
+export function heapSort(arr: number[]) {
+    let maxHeap = Utils.buildMaxHeap(arr);
+    let lastElt = arr.length - 1;
+    while (lastElt > 0) {
+        maxHeap = Utils.swap(maxHeap, 0, lastElt);
+        maxHeap = Utils.heapify(maxHeap, 0, lastElt);
+        lastElt -= 1;
+    }
+    return maxHeap;
+}
+
+/**
+ * Quicksort
+ */
+export function quickSort(arr: number[], low:number, high:number) {
+    if (low < high) {
+        const partitionIndex = Utils.partition(arr, low, high);
+        quickSort(arr, low, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, high);
+        return arr;
+    }
+}
+
+/**
+ * Bubble sort
+ */
+export function bubbleSort(arr: number[]) {
+    for (let i = 0, len = arr.length - 1; i < len; i += 1) {
+        for (let j = 0, l = arr.length - i - 1 ; j < l; j += 1) {
+            if (arr[j] > arr[j + 1]) {
+                Utils.swap(arr, j, j + 1);
+            }
+
+        }
+
+    }
+    return arr;
+}
